7edb0b1c552f628f4e9b06ac60319563fede63cc
AbrahamTesla <manlangitv@gmail.com>
2018-07-28 23:52:43 -0400

    modify

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,12 +13,14 @@ app.post('/api/AllFriends',function(req,res){
     var friendsScores = req.body.scores;
     var scoresArray = [];
     var bestMatch =0;
-
+    
+    console.log("hello");
     for(var i=0;i<friendsArray.length;i++){
         var scoresDiff = 0;
          
         for(var k=0;k<friendsScores.length;k++){
-            scoresDiff += (Math.abs(parseInt(friendsArray[i].scores[k]) - parseInt(friendsScores[k])));   
+            scoresDiff += (Math.abs(parseInt(friendsArray[i].scores[k]) - parseInt(friendsScores[k]))); 
+
         }
     
     scoresArray.push(scoresDiff);
@@ -41,7 +43,9 @@ for (var i =0;i<scoresArray.length;i++){
  
 // return the best match result
 var bestResult = friendsArray[bestMatch];
+console.log(bestResult);
 res.json(bestResult);
+
 
 //pushes the new submission into the friendsArray
 friendsArray.push(req.body);
